1bbe522d685223b84fdc7f140c83f19836abbe39
Scott Burnette <scott01019@gmail.com>
2018-03-15 18:47:52 -0700

    A new commit

--- core_location/ViewController.swift
+++ core_location/ViewController.swift
@@ -12,26 +12,31 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
+    var timer : Timer = Timer()
+    var totalDistance : CLLocationDistance = 0.0
+    var minAltitude : CLLocation?
+    var maxAltitude : CLLocation?
+    var lastLocation : CLLocation?
     @IBOutlet weak var mapView : MKMapView!
     var locationManager : CLLocationManager = CLLocationManager()
-    var path : [CLLocation] = [CLLocation]()
     
     @IBAction func startButtonPressed(_ sender: UIBarButtonItem) {
         mapView.showsUserLocation = true
         locationManager.startUpdatingLocation()
+        lastLocation = locationManager.location!
+        minAltitude = lastLocation
+        maxAltitude = lastLocation
+        pin(location: lastLocation!)
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIBarButtonItem) {
+        mapView.removeOverlays(mapView.overlays)
+        mapView.removeAnnotations(mapView.annotations)
     }
     
     @IBAction func stopButtonPressed(_ sender: UIBarButtonItem) {
+        pin(location: locationManager.location!)
         locationManager.stopUpdatingLocation()
-        let startLocation = path[0]
-        let endLocation = path[path.count - 1]
-        
-        pin(location: startLocation)
-        pin(location: endLocation)
-        
-        for i in 0...path.count - 2 {
-            drawRedLine(start: path[i], end: path[i+1])
-        }
     }
     
     func pin(location: CLLocation) {
@@ -41,40 +46,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     func drawRedLine(start: CLLocation, end: CLLocation) {
-        let sourceLocation = CLLocationCoordinate2D(latitude: start.coordinate.latitude, longitude: start.coordinate.longitude)
-        let destinationLocation = CLLocationCoordinate2D(latitude: end.coordinate.latitude, longitude: end.coordinate.longitude)
-        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation)
-        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation)
-        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
-        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
-        
-        // 7.
-        let directionRequest = MKDirectionsRequest()
-        directionRequest.source = sourceMapItem
-        directionRequest.destination = destinationMapItem
-        directionRequest.transportType = .walking
-        
-        // Calculate the direction
-        let directions = MKDirections(request: directionRequest)
-        
-        // 8.
-        directions.calculate {
-            (response, error) -> Void in
-            
-            guard let response = response else {
-                if let error = error {
-                    print("Error: \(error)")
-                }
-                
-                return
-            }
-            
-            let route = response.routes[0]
-            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
-            
-            let rect = route.polyline.boundingMapRect
-            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
-        }
+        let coordinates = [ CLLocationCoordinate2D(latitude: start.coordinate.latitude, longitude: start.coordinate.longitude),
+                            CLLocationCoordinate2D(latitude: end.coordinate.latitude, longitude: end.coordinate.longitude) ]
+        let polyline = MKPolyline(coordinates: coordinates, count: 2)
+        mapView.add(polyline)
     }
     
     override func viewDidLoad() {
@@ -83,13 +58,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
-        
         mapView.delegate = self
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
-        let latestLocation: CLLocation = locations[locations.count - 1]
-        path.append(latestLocation)
+        totalDistance = totalDistance + (lastLocation?.distance(from: locations[locations.count - 1]))!
+        
+        if let last = lastLocation {
+            drawRedLine(start: last, end: locations[locations.count - 1])
+        }
+        lastLocation = locations[locations.count - 1]
+        
+        if Double((lastLocation?.altitude)!) > Double((maxAltitude?.altitude)!) {
+            maxAltitude = lastLocation
+        }
+        if Double((lastLocation?.altitude)!) < Double((minAltitude?.altitude)!) {
+            minAltitude = lastLocation
+        }
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
